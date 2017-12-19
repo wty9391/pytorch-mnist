682a7b3ddc6ad83c20c272c7b044e691557039d9
wty <272076302@qq.com>
2017-12-19 16:31:30 +0800

    init

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import numpy
 from flask import Flask, jsonify, render_template, request
 from PIL import Image
+
+import torch
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+
+import LeNet
 # webapp
 app = Flask(__name__)
 
-def predict_with_pretrain_model(sample):
-	'''
-	Args:
-		sample: A integer ndarray indicating an image, whose shape is (28,28).
-
-	Returns:
-		A list consists of 10 double numbers, which denotes the probabilities of numbers(from 0 to 9).
-		like [0.1,0.1,0.2,0.05,0.05,0.1,0.1,0.1,0.1,0.1].
-	'''
-	pass
+def predict_with_pretrain_model(sample,model):
+    """
+    Args:
+        sample: A integer ndarray indicating an image, whose shape is (28,28).
+    
+    Returns:
+        A list consists of 10 double numbers, which denotes the probabilities of numbers(from 0 to 9).
+        like [0.1,0.1,0.2,0.05,0.05,0.1,0.1,0.1,0.1,0.1].
+    """
+    #In MNIST dataset pixel values are 0 to 255.
+    #0 means background (white), 255 means foreground (black). 
+    #In our dataset, 0 means black, 255 means white
+    sample = -sample + 255
+    img = Image.fromarray(sample)
+    transform = transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])
+    sample = transform(img).float()
+    sample = Variable(sample, volatile=True)
+    sample = sample.unsqueeze(0)
+    out = model.predict(sample)
+    return out.data[0].tolist()
 
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
+    model = LeNet.Net()
+    model.load_state_dict(torch.load('./results/model.pt'))
     input = ((numpy.array(request.json, dtype=numpy.uint8))).reshape(28, 28)
-    output = predict_with_pretrain_model(input)
+    output = predict_with_pretrain_model(input,model)
     return jsonify(results=output)
 
 
---
+++ train.py
@@ -0,0 +1,118 @@
+import argparse
+import torch
+from torchvision import datasets, transforms
+
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import LeNet
+
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                    help='input batch size for training (default: 256)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=20, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--save-path', type=str, default='./results/model.pt', metavar='P',
+                    help='where to save the trained model')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+
+def train(epoch,model,train_loader,optimizer):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+def test(model,test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        
+        output = model(data)
+        test_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./resources', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+    
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./resources', train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    
+    
+model = LeNet.Net()
+#If you need to move a model to GPU via .cuda(), 
+#please do so before constructing optimizers for it. 
+#Parameters of a model after .cuda() will be different objects with those before the call.
+if args.cuda:
+    model.cuda()
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+for epoch in range(1, args.epochs + 1):
+    train(epoch,model,train_loader,optimizer)
+    test(model,test_loader)
+    
+    
+torch.save(model.state_dict(), args.save_path)
+print("The model has been saved in [{}]".format(args.save_path))
+    
+    
+    
+    
+    
+    
+    
+    
